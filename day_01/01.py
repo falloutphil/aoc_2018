6f9168ea30517a2558c7327cc42380cf17295a23
phil <phil@beadling.co.uk>
2019-10-11 23:46:32 +0100

    python solution 1b

--- day_01/01.py
+++ day_01/01.py
@@ -1,4 +1,13 @@
 #!/usr/bin/env python3
 
-freqs = (int(i) for i in open('input.txt'))
+freqs = [int(i) for i in open('input.txt')]
 print(sum(freqs))
+
+from itertools import cycle, accumulate
+from collections import Counter
+c = Counter()
+for i in accumulate(cycle(freqs)):
+    if c[i] > 0:
+        print(i)
+        break
+    c[i] += 1
