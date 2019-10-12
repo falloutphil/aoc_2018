c07439fde4926fd4244391b32edd45706770bb65
phil <phil@beadling.co.uk>
2019-10-12 13:37:26 +0100

    day 2b

--- day_02/02.py
+++ day_02/02.py
@@ -1,10 +1,15 @@
 #!/usr/bin/env python3
 from collections import Counter
+from itertools import combinations
+
 
 #ids with exactly 2 repeated letters, then exactly 3 repeated letters
 lines = open('input.txt').read().split('\n')
 threes, twos = 0, 0
 for line in lines:
+    # Would be more efficient if
+    # the counter short-circuits on
+    # finding both 3s and 2s
     c = Counter(line)
     counts = dict(c.most_common()).values()
     if 3 in counts:
@@ -13,3 +18,8 @@ for line in lines:
         twos += 1
 
 print(twos*threes)
+
+for pair in combinations(lines, 2):
+    if [c1 == c2 for c1, c2 in zip(*pair)].count(False) == 1:
+        print(''.join([ c1 for c1, c2 in zip(*pair) if c1==c2 ]))
+        break
