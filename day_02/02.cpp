1b4038d180adda91c446a2a97a595e8555db419c
phil <phil@beadling.co.uk>
2019-10-23 23:58:23 +0100

    refactor

--- day_02/02.cpp
+++ day_02/02.cpp
@@ -1,41 +1,41 @@
 #include <fstream>
 #include <iostream>
-#include <vector>
-#include <iterator>
 #include <unordered_map>
 #include <boost/range/adaptors.hpp>
+#include <boost/range/algorithm/find.hpp>
 
 // hint: g++ 02.cpp -O2 -o cpp02
-
 
 using namespace std;
 using namespace boost;
 
 int main()
 {
-    vector<string> line;
     ifstream ifs("input.txt", ifstream::in);
-    istream_iterator<string> input(ifs);
-    copy(input, istream_iterator<string>(),
-         back_inserter(line));
-
+    istream_iterator<string> input(ifs), eos;
     unordered_map<char,int> char_count;
-    unsigned int twos = 0;
-    unsigned int threes = 0;
+    unsigned int twos, threes = 0;
 
-    for (string s : line)
+    do
     {
-        for (char c : s)
+        for (const char c : *input)
+            // Increment count for each occurrence
+            // of a char c in each line of the input
             ++char_count[c];
 
-        auto counts = adaptors::values(char_count);
-        if (find(counts.begin(), counts.end(), 2) != counts.end())
+        // Get all the counts for the line
+        // We don't care about which keys we counted
+        const auto& counts = adaptors::values(char_count);
+        // If any char occurred 2 or 3 times, increment
+        if (find(counts, 2) != counts.end())
             ++twos;
-        if (find(counts.begin(), counts.end(), 3) != counts.end())
+        if (find(counts, 3) != counts.end())
             ++threes;
 
+        // Delete counts to reuse for next line
         char_count.clear();
     }
+    while(++input != eos); // is next line end of stream?
 
     cout << twos*threes << endl;
 
