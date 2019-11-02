47802c5c23dfd6649086e86a2c56574aeb7f60f9
phil <phil@beadling.co.uk>
2019-11-02 06:23:37 +0000

    mapcar faster than loading seq.  permutation ideas.

--- day_02/02.el
+++ day_02/02.el
@@ -1,8 +1,20 @@
 #!emacs --script
 
-;; Runs in ~0.05 secs
+;; Runs in ~0.03 secs
 
-(require 'seq)
+(defun permutations (bag)
+  "Return a list of all the permutations of the input."
+  (if (null bag) '(())
+    ;; Otherwise, take an element, e, out of the bag.
+    ;; Generate all permutations of the remaining elements,
+    ;; And add e to the front of each of these.
+    ;; Do this for all possible e to generate all permutations.
+    (mapcan #'(lambda (e)
+                (mapcar (lambda (p) (cons e p))
+                        (permutations
+                         (remove e bag))))
+            bag)))
+
 
 (with-temp-buffer
   (insert-file-contents "input.txt")
@@ -14,10 +26,13 @@
         (twos 0)
         (threes 0))
     (dolist (line lines)
-      (seq-map (lambda (key) (puthash key (+ (gethash key myhash 0) 1) myhash)) line)
+      (mapcar (lambda (key) (puthash key (+ (gethash key myhash 0) 1) myhash)) line)
       (let ((vals '()))
         (maphash (lambda (_ v) (push v vals)) myhash)
         (when (member 2 vals) (setq twos (+ twos 1)))
         (when (member 3 vals) (setq threes (+ threes 1)))
         (clrhash myhash)))
     (print (* twos threes))))
+
+
+;;(print (permutations '(a b c)))
